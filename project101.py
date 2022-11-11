ff730ca398b53770ca8dbd8abe95c40d4120f74c
booksssss <86739545+booksssss@users.noreply.github.com>
2022-11-10 19:47:18 -0800

    Update project101.py

--- project101.py
+++ project101.py
@@ -6,11 +6,45 @@ response == "y"
 while response =="y":
   no = random.randint(1,6)
   if no==1:
-    print("[---]")
-    print("[   ]")
-    print("[ 0 ]")
-    print("[0 0]")
+    print("[-----]") 
+    print("[     ]") 
+    print("[  0  ]") 
+    print("[     ]") 
+    print("[-----]")
+  if no==2:
+    print("[-----]") 
+    print("[ 0   ]") 
+    print("[     ]") 
+    print("[   0 ]") 
+    print("[-----]")
+  if no==3:
+    print("[-----]") 
+    print("[     ]") 
+    print("[ 000 ]") 
+    print("[     ]") 
+    print("[-----]")
+  if no==4:
+    print("[-----]") 
+    print("[0   0]") 
+    print("[     ]") 
+    print("[0   0]") 
+    print("[-----]")
+  if no==5:
+    print("[-----]") 
+    print("[0   0]") 
+    print("[  0  ]") 
+    print("[0   0]") 
+    print("[-----]")
+  if no==6:
+    print("[-----]") 
+    print("[0   0]") 
+    print("[ 0 0 ]") 
+    print("[0   0]") 
+    print("[-----]")
+
    
   
   response = input("press y to roll the dice again and n to exit")
+
+
 
